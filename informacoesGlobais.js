051acc8a6150ba1e777981983e5259b31c6d6f88
mateus-valenga <mateus.valenga@escola.pr.gov.br>
2024-10-01 19:54:24 -0300

    Update informacoesGlobais.js

--- informacoesGlobais.js
+++ informacoesGlobais.js
@@ -1,5 +1,5 @@
 
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/mateusvalenga/api/main/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
